19a4e0392cb25063c67cd60de650ec908f2bbc5d
Вячеслав Иванов <slavik_ivanov_2015@bk.ru>
2024-10-18 01:28:54 +0300

    new file and correction

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -7,5 +7,5 @@ class IsAuthorOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         """Проверяет авторизован ли пользователь."""
-        return (request.method in permissions.SAFE_METHODS or
-                obj.author == request.user)
+        return (request.method in permissions.SAFE_METHODS
+                or obj.author == request.user)
